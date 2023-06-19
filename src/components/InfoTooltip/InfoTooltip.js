c998bd20d0adc442726d399f9844af2511cbfa21
Anton Rumyantsev <blush3d@ya.ru>
2023-06-19 07:18:40 +0300

    initial commit

--- src/components/InfoTooltip/InfoTooltip.js
+++ src/components/InfoTooltip/InfoTooltip.js
@@ -10,14 +10,14 @@ export function InfoTooltip(props) {
         <div className={`popup ${props.isOpen && "popup_opened"}`}>
             <div className="popup__container">
                 <div className="popup__wrapper">
-                    <h2 className={`popup__heading ${props.icon && "popup__heading_error"}`}>{!props.icon
+                    <h2 className={`popup__heading ${!props.icon && "popup__heading_error"}`}>{props.icon
                         ? `Форма успешно отправлена`
                         : `Ошибка`}
                     </h2>
-                    {props.icon && <button onClick={props.onClose} className="popup__close-button" type="button" aria-label="кнопка закрыть"></button>}
+                    {!props.icon && <button onClick={props.onClose} className="popup__close-button" type="button" aria-label="кнопка закрыть"></button>}
                 </div>
-                <div className={`popup__icon ${!props.icon && 'popup__icon_success'}`}></div>
-                {!props.icon ? <button onClick={handleClick} type="button" className="button popup__button">На главную</button>
+                <div className={`popup__icon ${props.icon && 'popup__icon_success'}`}></div>
+                {props.icon ? <button onClick={handleClick} type="button" className="button popup__button">На главную</button>
                     : <button onClick={props.onClose} type="button" className="button popup__button popup__button_close">Закрыть</button>}
             </div>
         </div>
